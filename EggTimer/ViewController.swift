c627b88647243a9e539d282ca90bb7e338c58aa9
김동호 <68005740+KimDongHo-Iisan@users.noreply.github.com>
2021-06-21 16:54:13 +0900

    타이머 만들기

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,18 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // 사전 형식으로 바꾸기
-    let eggTimes = ["Soft": 5, "Medium" :7, "Hard" : 12]
+    // 분을 초로 정의
+    let eggTimes = ["Soft": 300, "Medium" : 420, "Hard" : 7]
+    
+    var secondsRemaining = 60
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
         let hardness = sender.currentTitle! //sender.currentTitle에 언래핑을 하면 해결될것이라 생각했지만,
         
-        print(eggTimes[hardness]) //여전히 옵셔널이 출력된다. 이유는 sender.currentTitle이 문제가 아닌 사전에서 찾을수 있다. 사전은 까다롭게 정확도를 따진다. 해서 키값이 대소문자가 달라도 키값이 아니라고 간주하기 때문에, 사용자가 제대로 입력할 것이라는 확신을 가지지 못해 옵셔널로 간주하게 된다. 해서 우리가 컴퓨터에게 제대로 값을 입력했다는 확신을 주어야한다.
+        secondsRemaining = eggTimes[hardness]!
         
-        let result = eggTimes[hardness]! //언래핑으로 값이 확실히 있다는 것을 알려준다.
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true) // timeInterval : 타이머를 얼마나 자주 실행할것인지 물어보는 것,매초 업데이트를 원하기 때문에 1.0 타이머를 반복할지 여부를 결정 repeats . #selector는 오브젝트 c시절에 사용하던것, 이를 사용할때는 @objc를 넣어야함.
         
-        print(result) // 옵셔널이 사라진다.
+    }
+    //매초마다 발생하는 함수
+    @objc func updateTimer(){
+        if secondsRemaining > 0 {  // 아직 시간이 있는지 확인
+            print("\(secondsRemaining) seconds.") // 남은 시간을 출력
+            secondsRemaining -= 1 //실행될때마다 -1이 됨
+        }
     }
     
 }
+
+
+
