a1483fb798e89a2ac01bc6a07df0eb49986de97f
김동호 <68005740+KimDongHo-Iisan@users.noreply.github.com>
2021-06-21 16:39:31 +0900

    사전 형식으로 변경하기 1

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,24 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    // 사전 형식으로 바꾸기
+    let eggTimes = ["Soft": 5, "Medium" :7, "Hard" : 12]
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
-        let hardness = sender.currentTitle
+        let hardness = sender.currentTitle //Soft, Medium, Hard
         
-        switch hardness {
-        case "Soft":
-            print(softTime)
-        case "Medium":
-            print(mediumTime)
-        case "Hard":
-            print(hardTime)
-        default:
-            print("Error")
-        }
+        print(eggTimes[hardness]) //이렇게만 적으면 에러가 발생한다. ? 타입으로 출력되기 때문이다. currnetTitle이 빈 타이틀을 출력할 가능성도 있기 때문에 ? 타입으로 간주한다. 
     }
     
 }
