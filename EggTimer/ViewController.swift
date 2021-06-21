ef519e6338b1df9916abd597174860390204c708
김동호 <68005740+KimDongHo-Iisan@users.noreply.github.com>
2021-06-22 00:32:35 +0900

    2

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,35 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //타이틀 레이블 생성.
+   
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     
-    
-    let eggTimes = ["Soft": 300, "Medium" : 420, "Hard" : 7]
-    
-    var secondsRemaining = 60
-    
-    var timer = Timer() // 타이머 변수 생성
+    let eggTimes = ["Soft": 3, "Medium" : 4, "Hard" : 7]
+    var timer = Timer()
+    var totalTime = 0 // 전체 걸리는 시간
+    var secondsPassed = 0 // 현재 지나간 시간
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
-        progressBar.progress = 1.0
-        timer.invalidate() //타이머를 중지해줌
+        timer.invalidate()
         let hardness = sender.currentTitle!
         
-        secondsRemaining = eggTimes[hardness]!
+        totalTime = eggTimes[hardness]! //전체 시간 대입
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
-    //매초마다 발생하는 함수
+    
     @objc func updateTimer(){
-        if secondsRemaining > 0 {
-            print("\(secondsRemaining) seconds.")
-            secondsRemaining -= 1
-        } else { // 만약 시간이 다 되어 0이 되면
-            timer.invalidate() //타이머를 중단하고
-            titleLabel.text = "Done" // 레이블의 텍스트를 Done으로 변경해라.
+        if secondsPassed < totalTime { // 지나간 시간이 전체 걸리는 시간 보다 작을때,
+            let percentageProgress = secondsPassed / totalTime // 진행률은 전체 걸리는 시간 / 지나간 시간.
+            progressBar.progress = Float(percentageProgress) // 진행률을 진행바에 대입. 진행률은 부동소수점임.  -> 하지만 이렇게 코드를 짜면 값이 나오지 않고 0만 나온다.
+            secondsPassed += 1 //걸리는 시간을 1씩 증가
+        } else {
+            timer.invalidate()
+            titleLabel.text = "Done"
         }
     }
     
