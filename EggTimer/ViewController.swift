58d06e02aa0cad3ca14f3ea61c0a05ffd92d8d4d
김동호 <68005740+KimDongHo-Iisan@users.noreply.github.com>
2021-06-21 11:27:04 +0900

    2

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,11 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // 달걀을 익히는 시간
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
     
-
-
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        print(sender.currentTitle) //현재 누른 버튼의 이름 출력
+        
+        let hardness = sender.currentTitle //Initialization of immutable value 'hardness' was never used; consider replacing with assignment to '_' or removing it -> 변수로 사용되지 않기 때문에, 변수 선언을 할 필요가 없다고 하는 뜻.
+        
+        
     }
     
 }
