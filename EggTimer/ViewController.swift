feade44e5d67f7a8185202fb328cc5c014f3212d
김동호 <68005740+KimDongHo-Iisan@users.noreply.github.com>
2021-06-21 16:42:23 +0900

    2

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -15,9 +15,13 @@ class ViewController: UIViewController {
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
-        let hardness = sender.currentTitle //Soft, Medium, Hard
+        let hardness = sender.currentTitle! //sender.currentTitle에 언래핑을 하면 해결될것이라 생각했지만,
         
-        print(eggTimes[hardness]) //이렇게만 적으면 에러가 발생한다. ? 타입으로 출력되기 때문이다. currnetTitle이 빈 타이틀을 출력할 가능성도 있기 때문에 ? 타입으로 간주한다. 
+        print(eggTimes[hardness]) //여전히 옵셔널이 출력된다. 이유는 sender.currentTitle이 문제가 아닌 사전에서 찾을수 있다. 사전은 까다롭게 정확도를 따진다. 해서 키값이 대소문자가 달라도 키값이 아니라고 간주하기 때문에, 사용자가 제대로 입력할 것이라는 확신을 가지지 못해 옵셔널로 간주하게 된다. 해서 우리가 컴퓨터에게 제대로 값을 입력했다는 확신을 주어야한다.
+        
+        let result = eggTimes[hardness]! //언래핑으로 값이 확실히 있다는 것을 알려준다.
+        
+        print(result) // 옵셔널이 사라진다.
     }
     
 }
